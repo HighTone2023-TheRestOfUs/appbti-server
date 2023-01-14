af5973b21abd0dfab3dddeeed63c89b8b22f9177
박병관 <85085375+codingbotPark@users.noreply.github.com>
2023-01-14 22:18:25 +0900

    [ADD] controller

--- src/controller/surveyController.js
+++ src/controller/surveyController.js
@@ -1,15 +1,46 @@
 import * as baseResponseStatus from "../response/baseResponseStatus"
 import db from "../db"
 
-export const addSurvey = async (req,res) => {
-    const {appName,property,value} = req.body;
 
-    console.log(appName,property,value)
-    
-    db.query(`select * from `)
+export const addApp = async (req,res) => {
+    const {appName,simplePoint,numberPoint,functionPoint,category} = req.body
 
-    return res.status(200).json({
-        ...baseResponseStatus.SUCCESS,
-        message:"성공"
-    })
+    try {
+        db.query(`insert into apps(appName,simplePoint,numberPoint,functionPoint,category) values(${appName},${simplePoint},${numberPoint},${functionPoint},${category})`)
+        return res.status(200).json({
+            ...baseResponseStatus.SUCCESS,
+            message:"앱 등록 성공"
+        })
+    } catch(err) {
+        return res.status(400).json({
+            ...baseResponseStatus.FAILURE,
+            message:"앱 등록 실패"
+        })
+    }
 }
+
+
+export const updateApp = async (req,res) => {
+    // simplePoint or numberPoint or functionPoint
+    const propertyList = ["simplePoint","numberPoint","functionPoint"]
+    const {appName,property,value} = req.body;
+    try{
+
+        if (propertyList.includes(property)) throw err
+        db.query(`update apps set ${property}=apps.${property}+${value} where=${appName}`)
+        return res.status(200).json({
+            ...baseResponseStatus.SUCCESS,
+            message:"속성 값 업데이트 성공"
+        })
+
+    }catch(err) {
+        return res.status(400).json({
+            ...baseResponseStatus.FAILURE,
+            message:"앱 평가 수정 실패"
+        })
+    }
+
+
+
+}
+
--- src/routers/surveyRouter.js
+++ src/routers/surveyRouter.js
@@ -2,6 +2,6 @@ import express from "express"
 import * as surveyController from "../controller/surveyController"
 
 const surveyRouter = express.Router()
-surveyRouter.post('/add',surveyController.addSurvey)
+surveyRouter.post('/add',surveyController.updateApp)
 
 export default surveyRouter
